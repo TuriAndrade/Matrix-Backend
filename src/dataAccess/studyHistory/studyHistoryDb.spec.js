e11e1533638dfe0ee03855d4483a1bf0e4e6f4b2
Turi Vasconcelos <turivasconcelos@gmail.com>
2021-09-03 11:44:35 -0300

    Study history db tests

--- src/dataAccess/studyHistory/studyHistoryDb.spec.js
+++ src/dataAccess/studyHistory/studyHistoryDb.spec.js
@@ -25,4 +25,77 @@ describe('Study history db', () => {
 
     expect(createdStudyHistory.id).toEqual(expect.any(Number));
   });
+
+  it('Should update study history by id', async () => {
+    const user = await insertUser();
+
+    const studyHistory = createStudyHistory(
+      createFakeStudyHistory({ userId: user.id })
+    );
+
+    const createdStudyHistory = await studyHistoryDb.create(
+      studyHistory.spread()
+    );
+
+    const update = updateStudyHistory(
+      createFakeStudyHistory({ userId: user.id })
+    );
+
+    const [numberOfUpdatedStudyHistories] = await studyHistoryDb.updateById({
+      id: createdStudyHistory.id,
+      ...update.spread(),
+    });
+
+    expect(numberOfUpdatedStudyHistories).toBe(1);
+  });
+
+  it('Should find study histories by user id', async () => {
+    const user = await insertUser();
+
+    const studyHistories = [];
+
+    for (let i = 0; i < 10; i++) {
+      studyHistories.push(
+        createStudyHistory(createFakeStudyHistory({ userId: user.id }))
+      );
+    }
+
+    const studyHistoriesIds = [];
+
+    await Promise.all(
+      studyHistories.map((studyHistory) =>
+        studyHistoryDb.create(studyHistory.spread())
+      )
+    ).then((result) =>
+      result.forEach((entry) => studyHistoriesIds.push(entry.id))
+    );
+
+    const createdStudyHistories = await studyHistoryDb.findByUserId({
+      userId: user.id,
+    });
+
+    expect(createdStudyHistories.length).toBe(10);
+
+    createdStudyHistories.forEach((studyHistory) => {
+      expect(studyHistoriesIds).toContain(studyHistory.id);
+    });
+  });
+
+  it('Should delete study history by id', async () => {
+    const user = await insertUser();
+
+    const studyHistory = createStudyHistory(
+      createFakeStudyHistory({ userId: user.id })
+    );
+
+    const createdStudyHistory = await studyHistoryDb.create(
+      studyHistory.spread()
+    );
+
+    const numberOfDeletedStudyHistories = await studyHistoryDb.deleteById({
+      id: createdStudyHistory.id,
+    });
+
+    expect(numberOfDeletedStudyHistories).toBe(1);
+  });
 });
